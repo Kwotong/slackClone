9be6f424d48a719a64d5c03a66258c45d57693dc
Jeremy <user@MacBook-Pro-de-USER.local>
2023-07-17 09:25:12 +0100

    added html for scale section

--- src/pages/Enterprise/Enterprise.js
+++ src/pages/Enterprise/Enterprise.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { HeaderEnterprise } from '../../containers/enterprise';
+import { HeaderEnterprise, Scale } from '../../containers/enterprise';
 
 function Enterprise () {
     return (
         <div>
             <HeaderEnterprise />
+            <Scale />
         </div>
     )
 }
