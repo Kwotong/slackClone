64231678b5f0d5bd1a8dff5e95aafc38e605c7f5
Jeremy <user@MacBook-Pro-de-USER.local>
2023-07-19 15:42:34 +0100

    added scale section on enterprise page

--- src/containers/enterprise/scale/Scale.jsx
+++ src/containers/enterprise/scale/Scale.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './scale.css';
 import check from '../../../assets/check.svg';
 import arrow from '../../../assets/lSBmc7tyNDRW9.svg';
 import fox from '../../../assets/fox.png';
@@ -6,18 +7,22 @@ import scale from '../../../assets/scale.jpeg';
 
 function Scale () {
 
-    const scaleData = ['Get peace of mind with 99.99% uptime SLA', 'Customize Slack to your business with unlimited workspaces and channels', 'Govern all your users and teams from a single place']
+    const scaleData = [
+    'Get peace of mind with 99.99% uptime SLA', 
+    'Customize Slack to your business with unlimited workspaces and channels', 
+    'Govern all your users and teams from a single place'
+    ]
 
     return (
         <div className='enterprise__scale section__padding'>
             <div className='enterprise__scale-content'>
-                <h4>scale</h4>
-                <h2>Created to meet the needs of global organizations</h2>
+                <h4 className='small-heading'>scale</h4>
+                <h2 className='section-heading'>Created to meet the needs of global organizations</h2>
                 {
                     scaleData.map((text, index) => {
                         return (
                         <div className='enterprise__scale-details' key={index}>
-                            <div className='enterprise__scale-tick'>
+                            <div className='bullets'>
                                 <img src={check} alt='tick' />
                             </div>
                             <p>{text}</p>
@@ -30,8 +35,8 @@ function Scale () {
                     <img src={arrow} alt='arrow img' />
                 </div>
                 <p className='enterprise__scale-quote'>“Slack has been the hammer that has helped us tear down the walls of silos. It has enabled us to position Slack as the operating system of collaboration across 194 countries and 171 offices, pulling us together like nothing else has.”</p>
-                <img src={fox} alt='fox img' />
-                <p><span>Jeff Dow</span>, Executive Vice President, Media and Broadcast Operations</p>
+                <img className='enterprise__scale-foximg' src={fox} alt='fox img' />
+                <p className='enterprise__scale-position'><span>Jeff Dow</span>, Executive Vice President, Media and Broadcast Operations</p>
             </div>
             <div className='enterprise__scale-img'>
                 <img src={scale} alt='scale img' />
