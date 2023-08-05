ed00c17de10b86a48a568ac62b308d06c1bbdcc2
Jeremy <user@MacBook-Pro-de-USER.local>
2023-08-05 08:23:14 +0100

    modified header of sales page and enterprise page

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
     {
         path: '/signup',
         element: <SignUp />
-    }, 
+    },
     {
         path: '/contact-sales',
         element: <ContactSales />
@@ -34,5 +34,30 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <RouterProvider router={router} />
+    <RouterProvider router={router} basename='https://kwotong.github.io/slackClone' />
 );
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './index.css';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { Enterprise, Pricing, SignIn, SignUp, ContactSales } from './pages';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// const AppRouter = () => (
+//   <BrowserRouter basename="/slackClone">
+//     <Routes>
+//       <Route path="/" element={<App />} />
+//       <Route path="/enterprise" element={<Enterprise />} />
+//       <Route path="/pricing" element={<Pricing />} />
+//       <Route path="/signin" element={<SignIn />} />
+//       <Route path="/signup" element={<SignUp />} />
+//       <Route path="/contact-sales" element={<ContactSales />} />
+//     </Routes>
+//   </BrowserRouter>
+// );
+
+// root.render(<AppRouter />);
